46b67dabf8e15ba48526c68077782714f0852e58
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-11-13 12:23:21 -0800

    Debugging file system for nest-validation

--- tools/lib/git.py
+++ tools/lib/git.py
@@ -93,12 +93,17 @@ def am(repo, patch_data, threeway=False, directory=None, exclude=None,
   if committer_email is not None:
     root_args += ['-c', 'user.email=' + committer_email]
   root_args += ['-c', 'commit.gpgsign=false']
-  # command = ['git'] + root_args + ['am'] + args + ['; git am --show-current-patch=diff']
-  command = 'git ' + ' '.join(root_args) + ' am ' + ' '.join(args)
+  command = ['git'] + root_args + ['am'] + args
+  # command = 'git ' + ' '.join(root_args) + ' am ' + ' '.join(args)
   # command = 'git ' + ' '.join(root_args) + ' am ' + ' '.join(args) + '; git am --show-current-patch=diff'
   print("Running git patch command...")
   with subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
-    proc.communicate(patch_data.encode('utf-8'))
+    stdout, stderr = proc.communicate(patch_data.encode('utf-8'))
+
+    # Print results
+    print("Output:", stdout.decode())
+    print("Errors:", stderr.decode())
+
     if proc.returncode != 0:
       print("Failed to apply patch!")
       raise RuntimeError(f"Command {command} returned {proc.returncode}")
